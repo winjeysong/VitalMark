e0e14b6cef030a0daa60a810f0baa20465928895
winjeysong <winjey-song@163.com>
2017-09-25 23:40:40 +0800

    Add createbtn

---
+++ app/components/CreateBtn/index.jsx
@@ -0,0 +1,26 @@
+/**
+ * component for creating new things
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+const propTypes = {
+    click: PropTypes.func.isRequired
+};
+
+function CreateBtn({ click }) {
+    return (
+        <button
+            onClick={click}
+            className="create-btn btn btn primary"
+            type="button"
+        >
+            新建
+        </button>
+    );
+}
+
+CreateBtn.propTypes = propTypes;
+
+export default CreateBtn;
